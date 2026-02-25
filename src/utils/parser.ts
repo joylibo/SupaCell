19c6871d984430d76d25c2b964151adc560ec48b
李勃 <258696131@qq.com>
2026-02-25 21:40:39 +0800

    chore(deps): replace xlsx with xlsx-js-style to resolve High severity Prototype Pollution and ReDoS vulnerabilities

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,5 +1,5 @@
 import Papa from 'papaparse';
-import * as XLSX from 'xlsx';
+import * as XLSX from 'xlsx-js-style';
 // @ts-ignore
 import readXlsxFile from 'read-excel-file/browser';
 import { v4 as uuidv4 } from 'uuid';
